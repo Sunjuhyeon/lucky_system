db57bb0fccc83a5d5f962d094ca0e60db2b11ff9
dongsam-E <dong2dong32@gmail.com>
2024-02-19 17:37:42 +0900

    메인 컴포넌트 관련 버튼 클릭 시 렌더링 되는 컴포넌트

---
+++ src/views/sub/Service.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export default function Service() {
+  return (
+    <div className='all_wrap'>
+      <p className='sub_tit'>럭키시스템의 서비스 범위</p>
+      <ul className='all_list'>
+        <li className='list1'>
+          <p className='list_tit'>빌딩경비</p>
+          <ul>
+            <li>바닥청소</li>
+            <li>바닥청소</li>
+            <li>바닥청소</li>
+          </ul>
+        </li>
+        <li className='list2'>
+          <p className='list_tit'>빌딩경비</p>
+          <ul>
+            <li>바닥청소</li>
+            <li>바닥청소</li>
+            <li>바닥청소</li>
+          </ul>
+        </li>
+        <li className='list3'>
+          <p className='list_tit'>빌딩경비</p>
+          <ul>
+            <li>바닥청소</li>
+            <li>바닥청소</li>
+            <li>바닥청소</li>
+          </ul>
+        </li>
+        <li className='list4'>
+          <p className='list_tit'>빌딩경비</p>
+          <ul>
+            <li>바닥청소</li>
+            <li>바닥청소</li>
+            <li>바닥청소</li>
+          </ul>
+        </li>
+        <li className='list5'>
+          <p className='list_tit'>빌딩경비</p>
+          <ul>
+            <li>바닥청소</li>
+            <li>바닥청소</li>
+            <li>바닥청소</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  )
+}
